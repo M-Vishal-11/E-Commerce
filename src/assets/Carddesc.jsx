a7892e025cbe68898b711031712202d03573be63
M Vishal <m.vishal1110@gmail.com>
2025-03-03 21:05:57 +0530

    Delete src directory

--- src/assets/Carddesc.jsx
+++
@@ -1,9 +0,0 @@
-function Carddesc(){
-    return(<div>
-        <h1  className="Card-box">Description</h1>
-        <p>..........................................</p>
-        <h2 className="p-2 inline-block"><b>Price: $300</b></h2>
-        <a href="" className="p-2 rounded-2xl bg-blue-600 text-white w-20">Buy</a>
-    </div>);
-}
-export default Carddesc;
--- src/assets/Cardh1.jsx
+++
@@ -1,4 +0,0 @@
-function Cardh1(){
-    return(<h1  className="Card-box" style={{fontSize:24}}>Title</h1>);
-}
-export default Cardh1;
